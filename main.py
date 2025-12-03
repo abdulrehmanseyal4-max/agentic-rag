18ff1b4da053b8e3f4fa14a781e3800b906a9771
Abdulrehman Seyal <abdulrehmanseyal4@gmail.com>
2025-12-03 19:17:14 +0500

    Add hallucination and answer grader

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from dotenv import load_dotenv
+from graph.graph import app
+
+load_dotenv()
+
 def main():
-    print("Hello from agentic-rag!")
+    print("Hello Advanced RAG")
+    print(app.invoke(input={"question": "what is agent memory?"}))
 
 
 if __name__ == "__main__":
